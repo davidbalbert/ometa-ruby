b197008fcfef521fc4bd0b0c85f387fa2a293607
David Albert <davidbalbert@gmail.com>
2014-09-06 15:06:28 -0400

    Make assertions more abstract

--- test/peg_test.rb
+++ test/peg_test.rb
@@ -4,74 +4,82 @@ require 'peg'
 
 module Peg
   class PegTest < Minitest::Test
+    def assert_matches(input, peg)
+      assert peg.match(input), "`peg' did not match `#{input}'"
+    end
+
+    def assert_doesnt_match(input, peg)
+      assert_nil peg.match(input), "`peg' matched `#{input}' when it shouldn't have"
+    end
+
     def test_string_rule
-      assert_equal "", Literal.new("hello").match("hello")
-      assert_equal " world", Literal.new("hello").match("hello world")
-      assert_nil Literal.new("hello").match("goodbye")
-      assert_nil Literal.new("hello world").match("hello")
+      assert_matches "hello", Literal.new("hello")
+      assert_matches "hello world", Literal.new("hello")
+      assert_doesnt_match "goodbye", Literal.new("hello")
+      assert_doesnt_match "hello", Literal.new("hello world")
     end
 
     def test_sequence_peg
       peg = Sequence.new(Literal.new("a"), Literal.new("b"))
-      assert_equal "", peg.match("ab")
-      assert_nil peg.match("ac")
+      assert_matches "ab", peg
+      assert_doesnt_match "ac", peg
     end
 
     def test_ordered_choice
       peg = OrderedChoice.new(Literal.new("a"), Literal.new("b"), Literal.new("ab"))
-      assert_equal "", peg.match("a")
-      assert_equal "", peg.match("b")
-      assert_equal "b", peg.match("ab")
-      assert_equal "bc", peg.match("abc")
-      assert_nil peg.match("cde")
+      assert_matches "a", peg
+      assert_matches "b", peg
+      assert_matches "ab", peg
+      assert_matches "abc", peg
+      assert_doesnt_match "cde", peg
     end
 
     def test_any
-      assert_equal "", Any.new.match("a")
-      assert_nil Any.new.match("")
+      assert_matches "a", Any.new
+      assert_doesnt_match "", Any.new
     end
 
     def test_not
-      assert_equal "", Not.new(Any.new).match("")
-      assert_equal "a", Not.new(Not.new(Any.new)).match("a")
+      assert_matches "", Not.new(Any.new)
+      assert_matches "a", Not.new(Not.new(Any.new))
     end
 
     def test_lookahead
       peg = Lookahead.new(Literal.new("ab"))
-      assert_equal "abc", peg.match("abc")
-      assert_nil peg.match("bbc")
+      assert_matches "abc", peg
+      assert_doesnt_match "bbc", peg
     end
 
     def test_maybe
       peg = Maybe.new(Literal.new("a"))
-      assert_equal "", peg.match("a")
-      assert_equal "b", peg.match("b")
+      assert_matches "a", peg
+      assert_matches "b", peg
     end
 
     def test_zero_or_more
       peg = ZeroOrMore.new(Literal.new("a"))
-      assert_equal "", peg.match("")
-      assert_equal "", peg.match("a")
-      assert_equal "", peg.match("aa")
+      assert_matches "", peg
+      assert_matches "a", peg
+      assert_matches "aa", peg
     end
 
     def test_one_or_more
       peg = OneOrMore.new(Literal.new("a"))
-      assert_nil peg.match("")
-      assert_equal "", peg.match("a")
-      assert_equal "", peg.match("aa")
+      assert_doesnt_match "", peg
+      assert_matches "a", peg
+      assert_matches "aa", peg
     end
 
     def test_grouping
-      assert_equal "", Grouping.new(Literal.new("abc")).match("abc")
+      assert_matches "abc", Grouping.new(Literal.new("abc"))
     end
 
     def test_chars
       peg = Characters.new(?a, ?b, ?c)
-      assert_equal "", peg.match(?a)
-      assert_equal "", peg.match(?b)
-      assert_equal "", peg.match(?c)
-      assert_nil peg.match(?d)
+      assert_matches "a", peg
+      assert_matches "b", peg
+      assert_matches "c", peg
+      assert_doesnt_match "d", peg
     end
 
     def test_grammar
@@ -79,9 +87,9 @@ module Peg
         rule :top, ["hello", :x], "world"
       end
 
-      assert_equal "", g.match("helloworld")
-      assert_equal "foo", g.match("helloworldfoo")
-      assert_nil g.match("hello")
+      assert_matches "helloworld", g
+      assert_matches "helloworldfoo", g
+      assert_doesnt_match "hello", g
     end
   end
 end
